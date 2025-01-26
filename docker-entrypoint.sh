c8260c9969cfe79838d2f0d9d031b2d01b918339
Angel Suarez <angel@rise-technologies.com>
2025-01-26 23:54:20 +0100

    Install wait-for-it script to enable readiness checks

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-mysqld &
-wait-for-it -t 30 localhost:3306 --
+mysqld 
+# wait-for-it -t 30 localhost:3306 --
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'myrootpassword';"
 mysql -e "FLUSH PRIVILEGES;"
 

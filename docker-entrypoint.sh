52f6d133692ba3a4f2aad8d7bca16df5d6e4565d
Angel Suarez <angel@rise-technologies.com>
2025-01-26 23:59:00 +0100

    Ensure /run/mysqld directory exists for MySQL socket binding

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
 
-mysqld 
+mysqld
 # wait-for-it -t 30 localhost:3306 --
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'myrootpassword';"
 mysql -e "FLUSH PRIVILEGES;"

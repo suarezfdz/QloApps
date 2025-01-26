257001fae581d857c9fb3937d31ba17a11b59298
Angel Suarez <angel@rise-technologies.com>
2025-01-26 23:38:08 +0100

    Use mysqld to start MySQL for Docker compatibility

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-service mysql start
+mysqld &
+wait-for-it -t 30 localhost:3306 --
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'myrootpassword';"
 mysql -e "FLUSH PRIVILEGES;"
 
